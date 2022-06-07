a48c9988429b9dc255e28f80b24c6e682028ec0b
SpookyParadise(SabiRuf) <spookyambassador98@gmail.com>
2022-06-07 11:21:21 +0300

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const mongoose = require('mongoose')
+const xhr2 = require('xhr2')
+const PORT = process.env.PORT || 3333
+const path = require('path')
+
+
+
+//encoding
+app.use(express.json())
+app.use(express.json({extended: true}))
+app.use(express.urlencoded())
+app.set('view engine', 'ejs')
+app.use(express(__dirname + '/views'))
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+app.get('/auth' , (req, res) => {
+    res.send('hi ')
+})
+
+
+//launch
+async function launch(){
+    try {
+        await mongoose.connect('mongodb+srv://Portfolio:portfolio9@portfoliobase.adlmq.mongodb.net/?retryWrites=true&w=majority')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+launch()
+app.listen(PORT, (req, res) => {
+    console.log(`http://localhost:${PORT}`);
+})
